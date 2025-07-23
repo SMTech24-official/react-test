47fabb59480430d81b6af0848c4631549b9e8c70
euhansarkar <euhansarkar@gmail.com>
2025-07-23 07:10:49 +0600

    euhan: change 22.

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h2>test from euhan sarkar 2</h2>
-      <h1>Vite + React</h1>
+      <h1>Vite + React 2</h1>
       <h1>Vite + React euhan hello world 2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
